72ebbfcf4a03a4027b83085ee14d39222372330f
David Houston <davidaehouston@gmail.com>
2025-08-22 14:49:07 +0100

    Adding skeleton Space Resource Service, Space Resource Controller tweaks

--- backend/src/main/java/dev/dvhstn/workfromhere/spaces/controller/SpaceResourceController.java
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/controller/SpaceResourceController.java
@@ -1,6 +1,7 @@
 package dev.dvhstn.workfromhere.spaces.controller;
 
 import dev.dvhstn.workfromhere.spaces.model.SpaceResource;
+import dev.dvhstn.workfromhere.spaces.service.SpaceResourceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,34 +10,51 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 public class SpaceResourceController {
 
-    @GetMapping
-    public ResponseEntity<List<SpaceResource>> getAllSpaces() {
-        return null;
+    private final SpaceResourceService spaceResourceService;
+
+    public SpaceResourceController(SpaceResourceService spaceResourceService) {
+        this.spaceResourceService = spaceResourceService;
     }
 
     @GetMapping
-    public ResponseEntity<SpaceResource> getSpaceById(@PathVariable long id) {
-        return null;
+    public ResponseEntity<List<SpaceResource>> getAllSpaces() {
+        return ResponseEntity.ok(spaceResourceService.getAllSpaces());
+    }
+
+    @GetMapping
+    public ResponseEntity<SpaceResource> getSpaceById(@PathVariable Long id) {
+        return ResponseEntity.ok(spaceResourceService.getSpaceById(id));
     }
 
     @PostMapping
     public ResponseEntity<SpaceResource> createSpace(@RequestBody SpaceResource spaceResource) {
-        return null;
+        URI createdSpaceURI = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .build()
+                .toUri();
+
+        return ResponseEntity.created(createdSpaceURI).body(spaceResourceService.createSpace(spaceResource));
     }
 
     @PutMapping
-    public ResponseEntity<Void> updateSpaceById(@PathVariable Long id, @RequestBody SpaceResource spaceResource) {
-        return null;
+    public ResponseEntity<Void> updateSpaceById(@PathVariable Long id, @RequestBody SpaceResource updatedResource) {
+        spaceResourceService.updateSpace(id, updatedResource);
+
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> deleteSpace(@PathVariable long id) {
-        return null;
+    public ResponseEntity<Void> deleteSpace(@PathVariable Long id) {
+        spaceResourceService.deleteSpaceById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/service/SpaceResourceService.java
@@ -0,0 +1,30 @@
+package dev.dvhstn.workfromhere.spaces.service;
+
+import dev.dvhstn.workfromhere.spaces.model.SpaceResource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SpaceResourceService {
+    public List<SpaceResource> getAllSpaces() {
+        return null;
+    }
+
+
+    public SpaceResource getSpaceById(Long id) {
+        return null;
+    }
+
+    public SpaceResource createSpace(SpaceResource spaceResource) {
+        return null;
+    }
+
+
+    public void updateSpace(Long id, SpaceResource updatedResource) {
+    }
+
+
+    public void deleteSpaceById(Long id) {
+    }
+}
