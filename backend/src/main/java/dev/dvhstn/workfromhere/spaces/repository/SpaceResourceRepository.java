68a72e1990b260bcb299e05e9246158848843c79
David Houston <davidaehouston@gmail.com>
2025-08-23 11:16:08 +0100

    Adding skeleton SpaceResourceRepository, Service tweaks

---
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/repository/SpaceResourceRepository.java
@@ -0,0 +1,7 @@
+package dev.dvhstn.workfromhere.spaces.repository;
+
+import dev.dvhstn.workfromhere.spaces.model.SpaceResource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpaceResourceRepository extends JpaRepository<SpaceResource, Long> {
+}
