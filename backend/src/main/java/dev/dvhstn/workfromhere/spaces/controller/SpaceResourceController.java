55836a4aff1732d0c53dcff1261216a9eef6c38d
David Houston <davidaehouston@gmail.com>
2025-09-27 10:53:19 +0100

    Adding UserDetailsService

--- backend/src/main/java/dev/dvhstn/workfromhere/spaces/controller/SpaceResourceController.java
+++ backend/src/main/java/dev/dvhstn/workfromhere/spaces/controller/SpaceResourceController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 public class SpaceResourceController {
